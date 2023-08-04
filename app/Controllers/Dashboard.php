85c27193ddc71aaab8f3c562fae4921279fec931
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2023-08-04 15:54:03 +0700

    benerin adminlte yang mecong

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,14 +1,23 @@
 <?php
 namespace App\Controllers;
+use App\Models\PegawaiModel;
+
 class Dashboard extends BaseController
 {
     public function index()
     {
+        $pegawai_model = new PegawaiModel();
+        $all_data_pegawai = $pegawai_model->findAll();
+
+        $data = [
+            'all_data_pegawai' => $all_data_pegawai
+        ];
+
         $header['title']='Dashboard';
         echo view('partial/header',$header);
         echo view('partial/top_menu');
         echo view('partial/side_menu');
-        echo view('dashboard');
+        echo view('dashboard', $data);
         echo view('partial/footer');
     }
 }
--- app/Views/partial/footer.php
+++ app/Views/partial/footer.php
@@ -22,12 +22,12 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
+<script src="adminLTE/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+<script src="adminLTE/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
+<script src="adminLTE/dist/js/demo.js"></script>
 </body>
 </html>
